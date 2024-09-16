25dfe875c285e4c77c58451f0871f8bcc9714162
CS <cahya.setyaa@gmail.com>
2024-09-16 18:18:06 +0700

    add logger function

---
+++ include/credis_common.h
@@ -0,0 +1,10 @@
+#ifndef CREDIS_COMMON_H
+#define CREDIS_COMMON_H
+
+#include <stdarg.h>
+#include <time.h>
+#include <stdio.h>
+
+void logger(const char* level, const char* format, ...);
+
+#endif // CREDIS_COMMON_H
---
+++ src/logger.c
@@ -0,0 +1,17 @@
+#include "../include/credis_common.h"
+
+void logger(const char* level, const char* format, ...) {
+    time_t now;
+    time(&now);
+    char time_buf[20];
+    strftime(time_buf, sizeof(time_buf), "%Y-%m-%d %H:%M:%S", localtime(&now));
+
+    va_list args;
+    va_start(args, format);
+
+    printf("[%s] %s: ", time_buf, level);
+    vprintf(format, args);
+    printf("\n");
+
+    va_end(args);
+}
